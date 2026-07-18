c0070afb208073a4c79dceeacb75eac4bf5ba569
Rishabhdev06 <rishabhgithub06@gmail.com>
2026-07-19 01:26:08 +0530

    Update CaptainHome.jsx

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -9,6 +9,7 @@ import { useEffect, useContext } from 'react'
 import { SocketContext } from '../context/SocketContext'
 import { CaptainDataContext } from '../context/CapatainContext'
 import axios from 'axios'
+import LiveTracking from '../components/LiveTracking'
 
 const CaptainHome = () => {
 
@@ -101,14 +102,14 @@ const CaptainHome = () => {
 
     return (
         <div className='h-screen'>
-            <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen z-10'>
                 <div className='font-bold text-xl tracking-tight'>Smart Ride</div>
-                <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+                <Link to='/captain/logout' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                     <i className="text-lg font-medium ri-logout-box-r-line"></i>
                 </Link>
             </div>
             <div className='h-3/5'>
-                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+                <LiveTracking />
 
             </div>
             <div className='h-2/5 p-6'>
@@ -132,3 +133,5 @@ const CaptainHome = () => {
 }
 
 export default CaptainHome
+
+      
