26174729b54fc978a976d963539bf11b8f7c29dc
Rishabhdev06 <rishabhgithub06@gmail.com>
2026-07-19 01:26:53 +0530

    Update CaptainLogout.jsx

--- frontend/src/pages/CaptainLogout.jsx
+++ frontend/src/pages/CaptainLogout.jsx
@@ -1,25 +1,26 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 export const CaptainLogout = () => {
-    const token = localStorage.getItem('captain-token')
     const navigate = useNavigate()
 
-    axios.get(`${import.meta.env.VITE_API_URL}/captains/logout`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }).then((response) => {
-        if (response.status === 200) {
-            localStorage.removeItem('captain-token')
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/captains/logout`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).finally(() => {
+            localStorage.removeItem('token')
             navigate('/captain-login')
-        }
-    })
+        })
+    }, [])
 
     return (
-        <div>CaptainLogout</div>
+        <div>Logging out...</div>
     )
 }
 
