e45fe21a76d6dcbdd915e9c2445f8527fbd793d7
Rishabhdev06 <rishabhgithub06@gmail.com>
2026-08-13 20:51:55 +0530

    Update UserProtectWrapper.jsx

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 const UserProtectWrapper = ({
     children
 }) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('userToken')
     const navigate = useNavigate()
     const { user, setUser } = useContext(UserDataContext)
     const [ isLoading, setIsLoading ] = useState(true)
