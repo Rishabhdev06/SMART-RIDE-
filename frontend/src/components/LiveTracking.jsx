bbdacc5e1a6d5d0f5ec6fc1358f37542ac7f3226
Rishabhdev06 <rishabhgithub06@gmail.com>
2026-08-14 11:45:20 +0530

    Update LiveTracking.jsx

--- frontend/src/components/LiveTracking.jsx
+++ frontend/src/components/LiveTracking.jsx
@@ -92,7 +92,7 @@ const LiveTracking = () => {
         >
 
             <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url={'https://maps.geoapify.com/v1/tile/osm-bright-smooth-slategray/{z}/{x}/{y}.png?apiKey=${import.meta.env.VITE_GEOAPIFY_API_KEY}'}
                 attribution='© OpenStreetMap contributors'
             />
 
